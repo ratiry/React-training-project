164f76bbfc87bafc419effa5c9e6a02aef438a4b
ratiry <ratiry@yandex.ru>
2022-08-06 13:30:55 +0300

    made Store and started doing the method dispatch

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
-import State, { subscribe } from './redux/State.js';
+// import State, { subscribe } from './redux/State.js';
+import Store from './redux/State.js';
 import './index.scss';
 import App from './App';
-import { addPost, changeNewPostText,changeNewMessageText,addMessage } from './redux/State.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
  let rerenderEntireTree=(State)=>{
   root.render(
     <React.StrictMode>
-      <App State={State} addPost={addPost} changeNewPostText={changeNewPostText} changeNewMessageText={changeNewMessageText} addMessage={addMessage}/>
+      <App State={State} addPost={Store.addPost.bind(Store)} changeNewPostText={Store.changeNewPostText.bind(Store)} changeNewMessageText={Store.changeNewMessageText.bind(Store)} addMessage={Store.addMessage.bind(Store)}/>
     </React.StrictMode>
   );
 }
 
-rerenderEntireTree(State);  
-subscribe(rerenderEntireTree)
+rerenderEntireTree(Store.getState());  
+Store.subscribe(rerenderEntireTree)
 
 reportWebVitals();
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,66 +1,69 @@
-
-let rerenderEntireTree=()=>{
-  
-}
-let State ={
-  Wall:{
-    DataPosts:[
-      {message:'dgddg', Likes:1500, id:1},
-      {message:'fgfgf',Likes :10, id:2},
-      {message:'ggggg',Likes:0, id:3},
-    ],
-    new_text:'',
+let Store={
+   _State :{
+    Wall:{
+      DataPosts:[
+        {message:'dgddg', Likes:1500, id:1},
+        {message:'fgfgf',Likes :10, id:2},
+        {message:'ggggg',Likes:0, id:3},
+      ],
+      new_text:'hh',
+    },
+    Dialogs:{
+      dataUsers:[
+        {name:'Jem',id:1},
+        {name:'Jack',id:2},
+        {name:'Jerry',id:3},
+        {name:'Quizzy',id:4, },
+      ],
+      dataMessages:[
+        { message:'Hi' , whose:'my',Name:'Name'},
+        { message:'Hi' , whose:'my',Name:'Name'},
+        { message:'Hi' , whose:'my',Name:'Name'},
+        { message:'gibberish', whose:'coversator',Name:'Name'},
+        { message:'fdghj', whose:'conversator', Name:'Name'},
+      ],
+      new_text_Dialogs:''
+    },
   },
-  Dialogs:{
-    dataUsers:[
-      {name:'Jem',id:1},
-      {name:'Jack',id:2},
-      {name:'Jerry',id:3},
-      {name:'Quizzy',id:4, },
-    ],
-    dataMessages:[
-      { message:'Hi' , whose:'my',Name:'Name'},
-      { message:'Hi' , whose:'my',Name:'Name'},
-      { message:'Hi' , whose:'my',Name:'Name'},
-      { message:'gibberish', whose:'coversator',Name:'Name'},
-      { message:'fdghj', whose:'conversator', Name:'Name'},
-    ],
-    new_text_Dialogs:''
+  _rerenderEntireTree(){},
+  getState(){
+    return this._State;
   },
-}
-Window.State=State;
+  dispatch(){},
+  subscribe(observer){
+    this._rerenderEntireTree = observer;
+  },
+  addPost(){
+    // debugger;
+    let post={
+      message:this._State.Wall.new_text,
+      Likes:0,
+      id:4
+    }
+    this._State.Wall.DataPosts.push(post);
+    this.changeNewPostText('');
+    this._rerenderEntireTree(this._State);
+  },
+  addMessage(){
+    let newMessage={
+      message:this._State.Dialogs.new_text_Dialogs,
+      whose:'my',
+      Name:'Name'
+    }
+    this._State.Dialogs.dataMessages.push(newMessage);
+    this.changeNewMessageText('')
+    this._rerenderEntireTree(this._State)
+  },
+  changeNewPostText(value){
+    this._State.Wall.new_text=value;
+    this._rerenderEntireTree(this._State); 
+  },
+  changeNewMessageText(value){
+    this._State.Dialogs.new_text_Dialogs=value;
+    this._rerenderEntireTree(this._State);
+  },
 
+}
 
-export let addPost =()=>{
-  let post={
-    message:State.Wall.new_text,
-    Likes:0,
-    id:4
-  }
-  State.Wall.DataPosts.push(post);
-  changeNewPostText('');
-  rerenderEntireTree(State);
-}
-export let addMessage=()=>{
-  let newMessage={
-    message:State.Dialogs.new_text_Dialogs,
-    whose:'my',
-    Name:'Name'
-  }
-  State.Dialogs.dataMessages.push(newMessage);
-  changeNewMessageText('')
-  rerenderEntireTree(State);
-}
-export let changeNewPostText=(value)=>{
- State.Wall.new_text=value;
- console.log(State);
- rerenderEntireTree(State); 
-}
-export let changeNewMessageText=(value)=>{
-  State.Dialogs.new_text_Dialogs=value;
-  rerenderEntireTree(State);
-}
-export let subscribe=(observer)=>{
-  rerenderEntireTree = observer;
-}
-export default State;
+Window.Store = Store;
+export default Store;
