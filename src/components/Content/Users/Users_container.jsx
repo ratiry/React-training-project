f70849e359278d09efb8f5bc3966fb6a5a1ae04f
ratiry <ratiry@yandex.ru>
2022-10-24 18:42:52 +0300

    started reducing MapDispatchToProps in Users

--- src/components/Content/Users/Users_container.jsx
+++ src/components/Content/Users/Users_container.jsx
@@ -77,46 +77,6 @@ let mapStateToProps=(state)=>{
     IsFetching:state.Users.IsFetching
   }
 }
-let mapDispatchToprops = (dispatch)=>{
-  return{
-    follow:(id)=>{
-      let action = followAC(id);
-      dispatch(action);
-    },
-    unfollow:(id)=>{
-      let action = unfollowAC(id);
-      dispatch(action);
-    },
-    setUsers:(users)=>{
-      let action = setUsersAC(users);
-      dispatch(action)
-    },
-    SetCurrentPage:(currentPage)=>{
-      let  action = SetCurrentPageAC(currentPage);
-      dispatch(action);
-    },
-    SetTotalCount:(TotalCount)=>{
-      let  action = SetTotalCountAC(TotalCount);
-      dispatch(action);
-    },
-    buttonForward:()=>{
-      let action = buttonForwardAC();
-      dispatch(action)
-    },
-    buttonBackward:()=>{
-      let action = buttonBackwardAC();
-      dispatch(action);
-    },
-    SetPages:()=>{
-      let action = SetPagesAC();
-      dispatch(action);
-    },
-    IsFetching_action:(IsFetching)=>{
-      let action= IsFetchingAC(IsFetching);
-      dispatch(action);
-    }
-  }
-}
 export  let Users_container = connect(mapStateToProps,{
   follow:followAC,
   unfollow:unfollowAC,
@@ -125,5 +85,6 @@ export  let Users_container = connect(mapStateToProps,{
   SetTotalCount:SetTotalCountAC,
   buttonForward:buttonForwardAC,
   SetPages:SetPagesAC,
-  IsFetching_action:IsFetchingAC
+  IsFetching_action:IsFetchingAC,
+  buttonBackward:buttonBackwardAC
 })(UsersAPI);
